49e6eb47d395ca7fe4fe843d5e084cbcae09d99b
Maxwell Ribeiro <maxwellmataribeiro@hotmail.com>
2021-05-31 20:36:10 -0300

    aula 270

--- Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -11,71 +11,9 @@ import { EventoService } from 'src/app/services/evento.service';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent implements OnInit {
-  modalRef: BsModalRef;
-  public eventos: Evento[] = [];
-  public eventosFiltrados: Evento[] = [];
-  public larguraImagem = 150;
-  public margemImagem = 2;
-  public exibirImagem = true;
-  private filtroListrado = '';
 
-  public get filtroLista(): string {
-    return this.filtroListrado;
-  }
+  ngOnInit(): void {
 
-  public set filtroLista(value: string) {
-    this.filtroListrado = value;
-    this.eventos = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
-  }
-
-  public filtrarEventos(filtrarPor: string): Evento[] {
-    filtrarPor = filtrarPor.toLocaleLowerCase();
-    return this.eventosFiltrados.filter((evento: { tema: string; local: string; }) =>
-                                                  evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
-                                                  evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1 );
-  }
-
-  constructor(private eventoService: EventoService,
-              private modalService: BsModalService,
-              private toastr: ToastrService,
-              private spinner: NgxSpinnerService) {
-                this.modalRef = new BsModalRef();
-              }
-
-  public ngOnInit(): void {
-    this.getEventos();
-    this.spinner.show();
-  }
-
-  public alterarImagem(): void {
-    this.exibirImagem = !this.exibirImagem;
-  }
-
-  public getEventos(): void {
-    this.eventoService.getEventos().subscribe({
-      next: (eventos: Evento[]) => {
-        this.eventos = eventos;
-        this.eventosFiltrados = this.eventos;
-      },
-      error: () => {
-        this.spinner.hide();
-        this.toastr.error('Erro ao Carregar os Eventos', 'Error!');
-      },
-      complete: () => this.spinner.hide()
-    });
-  }
-
-  openModal(template: TemplateRef<any>): void {
-    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
-  }
-
-  confirm(): void {
-    this.modalRef.hide();
-    this.toastr.success('O Evento foi deletado com Sucesso', 'Deletado');
-  }
-
-  decline(): void {
-    this.modalRef.hide();
   }
 
 }
--- Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-titulo',
@@ -12,11 +13,14 @@ export class TituloComponent implements OnInit {
   @Input() subtitulo = 'Desde 2021';
   @Input() botaoListar = false;
 
-  constructor() {
+  constructor(private router: Router) {
     this.titulo = '';
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  listar(): void {
+    this.router.navigate([`/${this.titulo.toLocaleLowerCase()}/lista`]);
   }
 
 }
